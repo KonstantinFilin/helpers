db730415927121ce3e72984a0de233f12e604e8d
Konstantin S. Filin <info@kfilin.ru>
2018-11-05 05:26:38 +0300

    Added developer .sh files

---
+++ metrics.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ksf/.config/composer/vendor/phpmetrics/phpmetrics/bin/phpmetrics --report-html=./_Metrics /var/www/github/helpers
---
+++ unittest.sh
@@ -0,0 +1,1 @@
+./vendor/bin/phpunit --colors=auto --bootstrap vendor/autoload.php --coverage-html ./_Coverage --whitelist ./src/Helpers tests/src/Helpers
