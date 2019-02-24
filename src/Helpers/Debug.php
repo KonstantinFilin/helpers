afb5ac5515a5d446e1f8da1d05801f51874f89b8
Konstantin S. Filin <info@kfilin.ru>
2019-02-24 17:30:41 +0300

    Added Debug class

---
+++ src/Helpers/Debug.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Helpers;
+
+class Debug 
+{
+    public static function getMemoryUsage() {
+        return [
+            memory_get_usage(),
+            memory_get_usage(true),
+            memory_get_peak_usage(),
+            memory_get_peak_usage(true)
+        ];
+    }
+    
+    public static function getFreeMemory() {
+        list( , $mem1) = explode(":", exec('cat /proc/meminfo | grep "MemFree"'));
+        return $mem1;
+    }
+    
+    public static function getStackTrace() {
+        $stack = debug_backtrace();       
+        $stackLines = [];
+
+        foreach ($stack as $step) {
+            $stackLines[] = sprintf(
+                "%s:%u %s::%s",
+                !empty($step["file"]) ? $step["file"] : "-= no file =-",
+                !empty($step["line"]) ? $step["line"] : 0,
+                !empty($step["class"]) ? $step["class"] : "-= no class =-",
+                !empty($step["function"]) ? $step["function"] : "-= no function =-"
+            );
+        }
+        
+        return $stackLines;
+    }
+}
